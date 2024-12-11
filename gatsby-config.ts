81c13cfe3c1e0314d4bbe49d6426c66b7648623c
Parth Mangtani <impmmmm@gmail.com>
2024-12-11 20:01:08 +0530

    💚 add pathPrefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `administration`,
     siteUrl: `https://thepmsquare.github.io/administration`,
   },
+  pathPrefix: "/administration",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
