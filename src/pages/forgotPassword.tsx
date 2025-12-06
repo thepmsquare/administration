1494f2889d86638e8b6e3b700f130bff691d66ef
Parth Mangtani <impmmmm@gmail.com>
2025-12-06 06:58:35 +0530

    ✨ forgotPassword: add email recovery functionality and improve UI interactions

--- src/pages/forgotPassword.tsx
+++ src/pages/forgotPassword.tsx
@@ -2,23 +2,33 @@ import "../stylesheets/forgotPassword.css";
 
 import { HeadFC, Link, navigate, PageProps } from "gatsby";
 import * as React from "react";
-import { GetUserRecoveryMethodsV0ResponseZ } from "squarecommonblhelper";
-import { UsernameInput } from "squarecomponents";
+import {
+  GetUserRecoveryMethodsV0ResponseZ,
+  RecoveryMethodEnum
+} from "squarecommonblhelper";
+import { PasswordInput, UsernameInput } from "squarecomponents";
 import CustomSnackbarStateType from "squarecomponents/types/CustomSnackbarStateType";
 import { z } from "zod";
 
-import { Button, CircularProgress, Typography } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  CircularProgress,
+  FormControlLabel,
+  TextField,
+  Typography
+} from "@mui/material";
 
 import Page from "../components/Page";
 import brandConfig from "../config/brand";
 import {
   ForgotPasswordState,
-  ForgotPasswordStateZ,
+  ForgotPasswordStateZ
 } from "../types/pages/ForgotPassword";
 import { IndexStateZ } from "../types/pages/Index";
 import {
   authenticationAdministrationBL,
-  authenticationCommonBL,
+  authenticationCommonBL
 } from "../utils/initialiser";
 
 export const Head: HeadFC = () => (
@@ -36,7 +46,7 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
 
   // state
   const [username, setUsername] = React.useState<string>(
-    state ? state.username : ""
+    state ? state.username : "",
   );
   const [snackbarState, changeSnackbarState] =
     React.useState<CustomSnackbarStateType>({
@@ -50,9 +60,15 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
   const [recoveryMethods, setRecoveryMethods] = React.useState<z.infer<
     typeof GetUserRecoveryMethodsV0ResponseZ.shape.data.shape.main
   > | null>(null);
-  const [selectedMethod, setSelectedMethod] = React.useState<string | null>(
-    null
-  );
+  const [selectedMethod, setSelectedMethod] =
+    React.useState<RecoveryMethodEnum | null>(null);
+
+  const [emailResetPasswordCodeInput, setEmailResetPasswordCodeInput] =
+    React.useState<string>("");
+  const [emailResetNewPassword, setEmailResetNewPassword] =
+    React.useState<string>("");
+  const [emailResetLogoutOtherSessions, setEmailResetLogoutOtherSessions] =
+    React.useState<boolean>(false);
   // Ref to track component mount status
   const isMountedRef = React.useRef<boolean>(true);
 
@@ -93,9 +109,8 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
       const accessTokenResponse =
         await authenticationAdministrationBL.generateAccessTokenV0();
       const accessToken = accessTokenResponse.data.main.access_token;
-      const userDetailsResponse = await authenticationCommonBL.getUserDetailsV0(
-        accessToken
-      );
+      const userDetailsResponse =
+        await authenticationCommonBL.getUserDetailsV0(accessToken);
       const username = userDetailsResponse.data.main.username;
       const user_id = userDetailsResponse.data.main.user_id;
       const newState = IndexStateZ.parse({
@@ -114,6 +129,72 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
     }
   };
 
+  const handleClearRecoveryMethods = () => {
+    setRecoveryMethods(null);
+    setSelectedMethod(null);
+    setUsername("");
+    setEmailResetPasswordCodeInput("");
+    setEmailResetNewPassword("");
+    setEmailResetLogoutOtherSessions(false);
+  };
+
+  const handleSendPasswordResetEmail = async () => {
+    try {
+      await authenticationCommonBL.sendResetPasswordEmailV0(username);
+      if (isMountedRef.current) {
+        changeSnackbarState({
+          isOpen: true,
+          message: "password reset email sent successfully.",
+          severity: "success",
+        });
+      }
+    } catch (e) {
+      if (isMountedRef.current) {
+        changeSnackbarState({
+          isOpen: true,
+          message: (e as Error).message,
+          severity: "error",
+        });
+      }
+    }
+  };
+
+  const handleEmailRecoverySubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const recoveryMethodsResponse =
+        await authenticationAdministrationBL.resetPasswordAndLoginUsingResetEmailCodeV0(
+          emailResetPasswordCodeInput,
+          username,
+          emailResetNewPassword,
+          emailResetLogoutOtherSessions,
+        );
+
+      // redirect to homepage
+      const indexState = IndexStateZ.parse({
+        user: {
+          user_id: recoveryMethodsResponse.data.main.user_id,
+          username: username,
+          access_token: recoveryMethodsResponse.data.main.access_token,
+        },
+      });
+
+      await navigate("/", { state: indexState });
+    } catch (e) {
+      if (isMountedRef.current) {
+        changeSnackbarState({
+          isOpen: true,
+          message: (e as Error).message,
+          severity: "error",
+        });
+      }
+    } finally {
+      if (isMountedRef.current) {
+        setIsFetchingRecovery(false);
+      }
+    }
+  };
   // useEffect
   React.useEffect(() => {
     checkForAccessToken();
@@ -148,12 +229,17 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
           label="Username"
           uniqueIdForARIA="forgot-password-username-input"
           variant="outlined"
-          others={{ required: true, disabled: isFetchingRecovery }}
+          others={{
+            required: true,
+            disabled: isFetchingRecovery || recoveryMethods !== null,
+          }}
         />
         <Button
           type="submit"
           variant="contained"
-          disabled={isFetchingRecovery || !username.trim()}
+          disabled={
+            isFetchingRecovery || !username.trim() || recoveryMethods !== null
+          }
         >
           {isFetchingRecovery ? (
             <>
@@ -164,11 +250,17 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
             "confirm"
           )}
         </Button>
+        <Button
+          color="inherit"
+          disabled={isFetchingRecovery}
+          onClick={handleClearRecoveryMethods}
+        >
+          clear
+        </Button>
+        <Button color="inherit" disabled={isFetchingRecovery}>
+          <Link to="/login">cancel</Link>
+        </Button>
       </form>
-
-      <Button color="inherit" disabled={isFetchingRecovery}>
-        <Link to="/login">cancel</Link>
-      </Button>
 
       {recoveryMethods && (
         <div
@@ -188,7 +280,7 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
                 variant="contained"
                 onClick={() => {
                   console.log(`Selected method: ${key}, value: ${value}`);
-                  setSelectedMethod(key);
+                  setSelectedMethod(key as RecoveryMethodEnum);
                 }}
                 disabled={isFetchingRecovery || !value}
                 sx={{ mt: 1 }}
@@ -197,7 +289,55 @@ const ForgotPasswordPage: React.FC<PageProps> = (props) => {
               </Button>
             </div>
           ))}
-          {selectedMethod && (
+          {selectedMethod === "EMAIL" && (
+            <>
+              <Typography sx={{ mt: 2 }}>
+                You selected: {selectedMethod}
+              </Typography>
+              <Button onClick={handleSendPasswordResetEmail}>
+                send code on email
+              </Button>
+              <form
+                onSubmit={handleEmailRecoverySubmit}
+                className="common-form"
+              >
+                <TextField
+                  label="code"
+                  value={emailResetPasswordCodeInput}
+                  onChange={(e) => {
+                    setEmailResetPasswordCodeInput(e.target.value);
+                  }}
+                />
+                <PasswordInput
+                  value={emailResetNewPassword}
+                  onChange={(e) => {
+                    setEmailResetNewPassword(e.target.value);
+                  }}
+                  label="new password"
+                  uniqueIdForARIA="new password"
+                />
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={emailResetLogoutOtherSessions}
+                      onChange={() => {
+                        setEmailResetLogoutOtherSessions(
+                          !emailResetLogoutOtherSessions,
+                        );
+                      }}
+                    />
+                  }
+                  label="logout other sessions"
+                />
+
+                <Button type="submit" variant="contained">
+                  Submit
+                </Button>
+              </form>
+            </>
+          )}
+          {selectedMethod === "BACKUP_CODE" && (
             <Typography sx={{ mt: 2 }}>
               You selected: {selectedMethod}
             </Typography>
